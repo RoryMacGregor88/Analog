8cbbe551cde1e7a3f49fd952f075c9743cae3a4b
RoryMacGregor88 <RoryMacGregor88@outlook.com>
2019-08-13 15:21:06 +0100

    Added ItemTest.java.

--- server/src/main/java/com/codeclan/server/repositories/ItemRepository.java
+++ server/src/main/java/com/codeclan/server/repositories/ItemRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.server.repositories;
 
-public interface ItemRepository {
+import com.codeclan.server.models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }

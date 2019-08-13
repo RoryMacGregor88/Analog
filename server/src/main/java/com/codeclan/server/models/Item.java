b0f87872e24a7f0228d6139f34c39650e7110ac3
RoryMacGregor88 <RoryMacGregor88@outlook.com>
2019-08-13 15:13:07 +0100

    Added repositories package and Item class.

--- server/src/main/java/com/codeclan/server/models/Item.java
+++ server/src/main/java/com/codeclan/server/models/Item.java
@@ -1,4 +1,87 @@
-package com.codeclan.server.models;
+package com.codeclan.analog.Analog.Models;
 
+import javax.persistence.*;
+
+import com.codeclan.analog.Analog.Enums.Decade;
+import com.codeclan.analog.Analog.Enums.Type;
+
+@Entity
+@Table(name="items")
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "type")
+    private Type type;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "serial number")
+    private int serialNumber;
+
+    @Column(name = "year")
+    private int year;
+
+    @Column(name = "decade")
+    private Decade decade;
+
+    @Column(name = "price")
+    private double price;
+
+    public Item(Type type, String brand, String model, int serialNumber, int year, Decade decade, double price) {
+        this.type = type;
+        this.brand = brand;
+        this.model = model;
+        this.serialNumber = serialNumber;
+        this.year = year;
+        this.decade = decade;
+        this.price = price;
+    }
+
+    public Item() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Decade getDecade() {
+        return decade;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }

6e0e6165d5cd543614b26ccc69344813cc3fa970
RoryMacGregor88 <RoryMacGregor88@outlook.com>
2019-08-15 00:36:08 +0100

    Installed Sass and added compile:sass script to package.json.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,12 +4,12 @@ import "../css/style.css";
 
 const NavBar = (props) => {
     return (
-        <nav>
-            <Link class="link" text="home" href="/"/>
-            <Link class="link" text="50s" href="/50s"/>
-            <Link class="link" text="60s" href="/60s"/>
-            <Link class="link" text="70s" href="/70s"/>
-            <Link class="link" text="80s" href="/80s"/>
+        <nav className="nav">
+            <Link text="home" href="/"/>
+            <Link text="50s" href="/50s"/>
+            <Link text="60s" href="/60s"/>
+            <Link text="70s" href="/70s"/>
+            <Link text="80s" href="/80s"/>
         </nav>
     )
 }
